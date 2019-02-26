19d241ded28ef84ebfd78ab640ba6e961659c816
Olga <revina.volga@gmail.ru>
2019-02-26 14:36:54 +0300

    feat: completed task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let configStr = [];
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    configStr[i] = bracketsConfig[i].join('');
+  }
+
+  let newstr = str;
+
+  for (var i = 0; i < str.length/2; i++) {
+    newstr = removeFromCycle(configStr, newstr);
+  }
+
+  if (newstr === '') {
+    return true;
+  } else {
+    return false;
+  }
+
+  function removeFromCycle(configStr, str) {
+    for (var i = 0; i < configStr.length; i++) {
+      str = str.replace(configStr[i], '');
+    }
+    return str;
+  }
+
 }
